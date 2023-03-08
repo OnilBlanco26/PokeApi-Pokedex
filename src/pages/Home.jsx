a6ddb2fb3ad2c2be9cdf8a156f26ccdb57b6552a
OnilBlanco26 <onilblanco26@gmail.com>
2023-03-08 17:15:30 -0300

    Fallas en el responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setTrainerGlobal } from "../store/slices/trainer.slice";
@@ -33,33 +33,33 @@ const Home = () => {
     const pokeballContainer = document.querySelector(".pokeball__container");
     const pokeballOpen = document.querySelector(".pokeball-open");
     const random = document.querySelector(".pokemon-random");
+    const audio = new Audio("../Home/openPokeballSound.mp3");
+    audio.loop = false;
+    audio.volume = 0.2;
 
-    console.log('me ejecute')
+    console.log("me ejecute");
     pokeball.classList.add("pokeball-hidden");
-    setInterval(() => {
+    setTimeout(() => {
       pokeballContainer.classList.add("pokeball__container-hidden");
     }, 1000);
-    setInterval(() => {
+    setTimeout(() => {
       pokeballOpen.classList.add("pokeball-open-visible");
+      audio.play();
     }, 3000);
-    // setInterval(() => {
-    //   pokeScreen.classList.add("poke-visible");
-    // }, 1000);
-    // setInterval(() => {
-    //   form.classList.add("form-visible");
-    // }, 4000);
-  //   setInterval(() => {
-  //     live4.classList.add("pokedex-visible");
-  //   }, 10000);
-  //   setInterval(() => {
-  //     live3.classList.add("poke-hidden");
-  //   }, 4000);
-    setInterval(() => {
+    setTimeout(() => {
       random.classList.add("random-visible");
-    }
-    , 4000);
-    console.log(pokemon)
-   };
+    }, 4000);
+
+    audio.addEventListener(
+      "ended",
+      () => {
+        audio.pause();
+        audio.currentTime = 0;
+      },
+      false
+    );
+    console.log(pokemon);
+  };
 
   return (
     <div className="home__container">
@@ -68,13 +68,50 @@ const Home = () => {
           <img className="poke-title" src="../Home/pokedexTitle.png" alt="" />
           <div className="pokeball__container">
             <h3 className="pokeball-text">Click To Open</h3>
-            <img onClick={clickToActive} className="pokeball" src="../Home/pokeball.png" alt="" />
+            <img
+              onClick={clickToActive}
+              className="pokeball"
+              src="../Home/pokeball.png"
+              alt=""
+            />
           </div>
           <div className="pokeball-open__container">
-            <img className="pokeball-open" src="../Home/pokeball-open.png" alt="" />
+            <img
+              className="pokeball-open"
+              src="../Home/pokeball-open.png"
+              alt=""
+            />
             <div className="pokemon-random">
-              <img src={pokemon?.sprites.other['official-artwork'].front_default} alt="" />
+              <div className="pokeQuiz__container">
+                <img
+                  className="pokemon-logo"
+                  src="../Home/Pokemon-logo.png"
+                  alt=""
+                />
+                <img
+                  className="pokemon-img"
+                  src={pokemon?.sprites.other["official-artwork"].front_default}
+                  alt=""
+                />
+                <div className="pokemon-interrogacion">
+                  <img
+                    className="pokemon-interrogacion--img"
+                    src="../Home/interrogacion.png"
+                    alt=""
+                  />
+                </div>
+                <div className="pokeQuiz-input__container">
+                    <input className="pokeQuiz-input" type="text" placeholder="Ingresar Nombre" />
+                    <div className="pokeQuiz-buttons">
+                        <button className="pokeQuiz-button">Adivinar</button>
+                        <button className="pokeQuiz-button">Saltar</button>
+                    </div>
+                </div>
+              </div>
             </div>
+          </div>
+          <div className="pokedex__container">
+            {/* <img className="pokedex" src="../Home/pokedex.png" alt="" /> */}
           </div>
         </div>
       </div>
@@ -83,4 +120,3 @@ const Home = () => {
 };
 
 export default Home;
-
