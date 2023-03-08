7c864cff3583c11356367609cda1af79bb835d5f
OnilBlanco26 <onilblanco26@gmail.com>
2023-03-08 13:28:21 -0300

    Pokeball float

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setTrainerGlobal } from "../store/slices/trainer.slice";
 import "../components/Pokedex/styles/home.css";
-import BattleVideo from "../components/BattleVideo";
 import axios from "axios";
 
 const Home = () => {
@@ -60,62 +59,14 @@ const Home = () => {
   return (
     <div className="home__container">
       <div className="home__container--info">
-        <div className="container-watchTV">
-          <img className="watchTV" src="../Home/watchTV.png" alt="" />
-          <h2 className="watchTV-title">WATCH NOW</h2>
-        </div>
         <div className="live__container">
-          <div className="live1">
-            <div className="text-container">
-              <div className="circle"></div>
-              <h3 className="text-live">LIVE</h3>
-            </div>
-            <img className="live1-img" src="../Home/Leon.gif" alt="" />
+          <img className="poke-title" src="../Home/pokedexTitle.png" alt="" />
+          <div className="pokeball__container">
+            <h3 className="pokeball-text">Click To Open</h3>
+            <img className="pokeball" src="../Home/pokeball.png" alt="" />
           </div>
-          <div className="live2">
-            <img className="live2-img" src="../Home/Profesora.gif" alt="" />
-          </div>
-          <div className="imgsPoke">
-            <img
-              id="pokeball"
-              onClick={clickToActive}
-              className="live-pokeball"
-              src="../Home/pokeball.png"
-              alt=""
-            />
-            <img
-              className="poke-screen"
-              src="../Home/pokeball-open.png"
-              alt=""
-            />
-
-            <div className="pokemon-random">
-              <img
-                src={pokemon?.sprites.other["official-artwork"].front_default}
-                alt=""
-              />
-            </div>
-            <div className="form__container">
-              <form className="home__form" onSubmit={handleSubmit} action="">
-                <input
-                  className="home__input"
-                  id="name"
-                  type="text"
-                  placeholder="Your name here..."
-                />
-                <button className="home__btn" type="submit">
-                  Start
-                </button>
-              </form>
-            </div>
-          </div>
-
-          <div className="live3">
-            <BattleVideo />
-            <h3 className="battle-title">POKEMON BATTLE</h3>
-          </div>
-          <div className="live4">
-            <img className="pokedex-img" src="../Home/pokedex.png" alt="" />
+          <div className="pokeball-open__container">
+            <img className="pokeball-open" src="../Home/pokeball-open.png" alt="" />
           </div>
         </div>
       </div>
